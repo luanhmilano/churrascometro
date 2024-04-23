197489ec457d69233fb572062b61ad7c15a20bea
Luan Henrique <luanhsantos14@gmail.com>
2024-04-23 11:25:30 -0300

    1. Setup inicial

---
+++ src/components/CalculadoraChurrasco.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CalculadoraChurrasco = () => {
+  return (
+    <div>CalculadoraChurrasco</div>
+  )
+}
+
+export default CalculadoraChurrasco
---
+++ src/components/ResultadoChurrasco.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ResultadoChurrasco = () => {
+  return (
+    <div>ResultadoChurrasco</div>
+  )
+}
+
+export default ResultadoChurrasco
