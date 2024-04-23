83e37fd59f71ea91f2872296bbaa3c0b95771013
Luan Henrique <luanhsantos14@gmail.com>
2024-04-23 12:29:48 -0300

    2. Criando página da calculadora

--- src/components/CalculadoraChurrasco.tsx
+++ src/components/CalculadoraChurrasco.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
+import { nomeAlimentos } from "../types"
+import { Formik, Field, Form } from "formik"
+import * as Yup from 'yup'
 
 const CalculadoraChurrasco = () => {
+  const navigate = useNavigate()
+
   return (
-    <div>CalculadoraChurrasco</div>
+    <div>
+      <Formik initialValues={{ pessoas: 0, selecaoAlimentos: [] }} onSubmit={() => {
+        navigate("/resultado")
+        console.log("teste")
+      }}>
+        <Form>
+          <div>
+            <label htmlFor="pessoas">Número de Pessoas</label>
+            <Field name="pessoas" type="number" />
+          </div>
+          <h2>Selecione os alimentos para o churrasco:</h2>
+          {Object.keys(nomeAlimentos).map((alimento) => (
+            <div key={nomeAlimentos.id}>
+              <Field type="checkbox" name="selecaoAlimentos" value={alimento} />
+              <label htmlFor="selecaoAlimentos">{nomeAlimentos[alimento]}</label>
+            </div>
+          ))}
+          <button type="submit">Calcular</button>
+        </Form>
+      </Formik>
+    </div>
   )
 }
 
--- src/pages/Calculadora.tsx
+++ src/pages/Calculadora.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import CalculadoraChurrasco from '../components/CalculadoraChurrasco'
 
 const Calculadora = () => {
   return (
-    <div>Calculadora</div>
+    <div>
+      <h1>Calculadora de Churrasco</h1>
+      <CalculadoraChurrasco />
+    </div>
   )
 }
 
